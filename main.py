72bbb98384b650869aa5d7df1d27d561ab22413d
nullip <107959430+nullip@users.noreply.github.com>
2022-12-09 11:50:59 -0600

    Update main.py
    Added some value checks for config, added debugging value for verbose output.

--- main.py
+++ main.py
@@ -3,37 +3,83 @@ import requests
 import json
 
 
+class bcolors:
+    HEADER = '\033[95m'
+    OK = '\033[92m'
+    WARNING = '\033[93m'
+    ERROR = '\033[91m'
+
+
 def main():
     ##################
     # START OF CONFIG
     ##################
 
-    emby_domain_name = 'https://x' #NO TRAILING /
-    emby_api_key = ''
+    emby_domain_name = '' # https://url or http://url
+    emby_api_key = '' # 32 Character API Key
 
-    kavita_domain_name = 'https://x' #NO TRAILING /
-    kavita_api_key = 'xxxxxx-xxxx-xxxx-xxxx-xxxxxxxx' #INCLUDE DASHES
-    roles = ['Download', 'Bookmark', 'Change Restriction']
-    libraries = [1, 2, 3] 
+    kavita_domain_name = '' # https://url or http://url
+    kavita_api_key = '' # xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx
+
+    roles = ['Download', 'Bookmark', 'Change Restriction'] # 'Download', 'Bookmark', 'Change Restriction'
+    libraries = [1, 2, 3] # 1, 2, 3, etc
+
+    debug = False
 
     ##################
     # END OF CONFIG
     ##################
 
+    # Config Validation
+    if bool(emby_domain_name) is False:
+        raise ValueError('CONFIG | Emby domain name value is blank.')
+    if bool(emby_api_key) is False:
+        raise ValueError(bcolors.WARNING + 'CONFIG | Emby API key value is blank.')
+    if bool(kavita_domain_name) is False:
+        raise ValueError(bcolors.WARNING + 'CONFIG | avita domain name value is blank.')
+    if bool(kavita_api_key) is False:
+        raise ValueError(bcolors.WARNING + 'CONFIG | Kavita api key value is blank.')
+    if bool(roles) is False:
+        raise ValueError(bcolors.WARNING + 'CONFIG | Kavita roles value is blank.')
+    if bool(libraries) is False:
+        raise ValueError(bcolors.WARNING + 'CONFIG | Kavita libraries value is blank.')
+    if '-' not in kavita_api_key:
+        raise ValueError(bcolors.WARNING + 'CONFIG | Kavita API Key value is missing dash characters.')
+    if '-' in emby_api_key:
+        raise ValueError(bcolors.WARNING + 'CONFIG | Emby API Key does not use special characters.')
+    if kavita_domain_name.endswith('/'):
+        raise ValueError(bcolors.WARNING + 'CONFIG | Kavita Domain name can not have a trailing slash.')
+    if emby_domain_name.endswith('/'):
+        raise ValueError(bcolors.WARNING + 'CONFIG | Kavita Domain name can not have a trailing slash.')
+
     # 1. Get JWT from Kavita Info
-    jwt = kavitapy.Reader(kavita_domain_name, kavita_api_key).token
+    try:
+        jwt = kavitapy.Reader(kavita_domain_name, kavita_api_key).token
+    except Exception:
+        print(bcolors.WARNING + 'JWT | Error retrieving Kavita JWT Token.')
 
     # 2. Get Emby Connect Users
-    connect_users = get_emby_users(emby_domain_name, emby_api_key)
-    print('emby connect users:', connect_users)
+    try:
+        connect_users = get_emby_users(emby_domain_name, emby_api_key)
+        if debug is True:
+            print('DEBUG | Emby Connect Users | ', connect_users)
+    except Exception:
+        print(bcolors.WARNING + 'Emby Connect | Error retrieving Emby Connect Users.')
 
     # 3. Get missing emails in Kavita
-    missing_emails = dupe_checker(kavita_domain_name, connect_users, jwt)
-    print('missing:', missing_emails)
+    try:
+        missing_emails = dupe_checker(kavita_domain_name, connect_users, jwt)
+        if debug is True:
+            print('DEBUG | Missing | ', missing_emails)
+    except Exception:
+        print(bcolors.WARNING + 'Missing Emails | Error generating Kavita JWT Token. Validate Kavita Config.')
 
     # 4. Post invite via Kavita
-    for email in missing_emails:
-        invite(kavita_domain_name, email, jwt, roles, libraries)
+    try:
+        for email in missing_emails:
+            invite(kavita_domain_name, email, jwt, roles, libraries)
+    except Exception:
+        print(bcolors.WARNING + 'Missing Emails | Error generating Kavita JWT Token. Validate Kavita Config.')
 
 
 ###########################
@@ -54,11 +100,9 @@ def get_emby_users(emby_domain_name, emby_api_key):
 
     if response.status_code == 200:
         data = json.loads(response.content.decode('utf-8'))
-        print(data)
         for item in data['Items']:
             if item.get('ConnectUserName') is not None:
                 connect_users.append(item.get('ConnectUserName'))
-
     return connect_users
 
 
@@ -110,7 +154,9 @@ def invite(domain_name, email, jwt, roles, libraries):
     response = requests.post(domain_name + '/api/Account/invite', headers=headers, json=json_data)
 
     if response.status_code == 200:
-        print("invite: send successfully")
+        print("Success | Invite | Send Successful", response.status_code)
+    else:
+        print("Error | Invite | Send Failure with error ", response.status_code)
 
 
 #
